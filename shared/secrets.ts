65af9b266567b5ad254f37ce65efac1659455e27
Luuk van Egeraat <l.vanegeraat@gmail.com>
2025-10-08 01:49:43 +0200

    chore: Avoid logging secret error, add warning comment

--- shared/secrets.ts
+++ shared/secrets.ts
@@ -12,6 +12,7 @@ export async function getSecrets(
 
     if (response.SecretString) return JSON.parse(response.SecretString);
   } catch (error) {
-    console.log('AWS Secrets error', error);
+    // Do not log the error, it might reveal secrets we can't put in the log
+    console.log('AWS Secrets error', SecretId, region);
   }
 }
