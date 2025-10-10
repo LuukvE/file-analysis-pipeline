68cd311e58f486270069cf84d15da01854492b31
Luuk van Egeraat <l.vanegeraat@gmail.com>
2025-10-10 21:07:04 +0200

    feat: Add monitor config and injectable secrets

--- aws/secrets.sh
+++ aws/secrets.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-SERVER_SECRETS=$(cat <<EOF
-{
-  "foo": "bar"
-}
-EOF
-)
-
 awslocal secretsmanager create-secret \
   --name server-secrets \
-  --secret-string "$SERVER_SECRETS" \
+  --secret-string "$(cat "$(dirname "$0")/secrets.server.json")" \
   --region eu-west-1 \
   >/dev/null
 
-PROCESSOR_SECRETS=$(cat <<EOF
-{
-  "foo": "bar"
-}
-EOF
-)
-
 awslocal secretsmanager create-secret \
   --name processor-secrets \
-  --secret-string "$PROCESSOR_SECRETS" \
+  --secret-string "$(cat "$(dirname "$0")/secrets.processor.json")" \
   --region eu-west-1 \
   >/dev/null
